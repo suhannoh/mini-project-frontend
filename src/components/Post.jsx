12cc101dd7ad0bb53dc058ab91bbd867e3a56639
suhannoh <suhannoh@gmail.com>
2025-12-16 16:10:20 +0900

    card CALSSNAME 꼬임 이슈 해결

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Post({id, title , like=0 , view}) {
   return (
-        <div className={view === "list" ? 'post' : "card"} >
-            <h2 className='post-id'> {id}</h2>
+        <div className={view === "list" ? 'post' : "post-card"} >
+            <h2 className='post-card-id'> {id}</h2>
             <h2 className='post-title'> {title} </h2>
             <h2 className='post-like'> ♥ {like} </h2>
         </div>
